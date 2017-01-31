1629ead7cb2b62b6595babfa43dd6ee944f4e000
Leo_hsu <leo_hsu@gemteks.com>
2017-01-31 15:56:12 +0800

    First commit

--- VoIPPushTest/AppDelegate.m
+++ VoIPPushTest/AppDelegate.m
@@ -7,8 +7,18 @@
 //
 
 #import "AppDelegate.h"
+#import <PushKit/PushKit.h>
+#import <UserNotifications/UserNotifications.h>
 
-@interface AppDelegate ()
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+static NSString *kNotificationIdentifier = @"NotificationIdentifier";
+
+@interface AppDelegate () <UNUserNotificationCenterDelegate, PKPushRegistryDelegate>
 
 @end
 
@@ -16,36 +26,183 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    [self setupRemoteNotification:application];
+    
+    [self voipRegistration];
+    
     return YES;
 }
 
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+
 }
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
 }
 
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+
 }
 
+// Register for VoIP notifications
+- (void) voipRegistration {
+    dispatch_queue_t mainQueue = dispatch_get_main_queue();
+    // Create a push registry object
+    PKPushRegistry * voipRegistry = [[PKPushRegistry alloc] initWithQueue: mainQueue];
+    // Set the registry's delegate to self
+    voipRegistry.delegate = self;
+    // Set the push type to VoIP
+    voipRegistry.desiredPushTypes = [NSSet setWithObject:PKPushTypeVoIP];
+}
+
+#pragma mark - PKPushRegistryDelegate
+
+// Handle updated push credentials
+- (void)pushRegistry:(PKPushRegistry *)registry didUpdatePushCredentials: (PKPushCredentials *)credentials forType:(NSString *)type {
+    // Register VoIP push token (a property of PKPushCredentials) with server
+    NSString *str = [NSString stringWithFormat:@"%@",credentials.token];
+    NSString *tokenStr = [[[str stringByReplacingOccurrencesOfString:@"<" withString:@""]
+                  stringByReplacingOccurrencesOfString:@">" withString:@""] stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSLog(@"token = %@", tokenStr);
+    NSLog(@"type = %@", type);
+}
+
+// Handle incoming pushes
+- (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(NSString *)type {
+    /*
+    payload = {
+        aps =     {
+            alert = "This is a message";
+            sound = default;
+            title = "VoIP Push Test";
+        };
+    }
+     */
+    // Process the received push
+    NSLog(@"payload = %@", payload.dictionaryPayload);
+    NSLog(@"type = %@", type);
+    
+    NSDictionary *payloadDict = payload.dictionaryPayload[@"aps"];
+    NSString *title = payloadDict[@"title"];
+    NSString *message = payloadDict[@"alert"];
+    
+    //present a local notification to visually see when we are recieving a VoIP Notification
+    if ([[UIApplication sharedApplication] applicationState] == UIApplicationStateBackground) {
+        [self registerLocalNotification:title message:message];
+    }
+    else {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Local notification" message:@"You got a message" preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+            [alert addAction:cancelAction];
+            [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
+        });
+    }
+    
+}
+
+- (void)pushRegistry:(PKPushRegistry *)registry didInvalidatePushTokenForType:(PKPushType)type {
+    NSLog(@"token invalidated");
+}
+
+#pragma mark - local notification
+
+- (void)registerLocalNotification:(NSString *)title message:(NSString *)msg {
+    // Send local notification after alerTime(in seconds)
+    NSInteger alerTime = 1;
+    
+    if( SYSTEM_VERSION_LESS_THAN( @"10.0" ) )
+    {
+        UILocalNotification *notification = [[UILocalNotification alloc] init];
+        if (notification) {
+            notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:alerTime];
+            notification.timeZone = [NSTimeZone defaultTimeZone];
+            notification.alertTitle = title;
+            notification.alertBody = msg;
+            NSDictionary *infoDict = [NSDictionary dictionaryWithObjectsAndKeys:@"notification_1", @"id", nil];
+            notification.userInfo = infoDict;
+            [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+        }
+    }
+    else
+    {
+        UNUserNotificationCenter* center = [UNUserNotificationCenter currentNotificationCenter];
+        
+        UNMutableNotificationContent* content = [[UNMutableNotificationContent alloc] init];
+        content.title = title;
+        content.body = msg;
+        content.sound = [UNNotificationSound defaultSound];
+        
+        UNTimeIntervalNotificationTrigger* trigger = [UNTimeIntervalNotificationTrigger
+                                                      triggerWithTimeInterval:alerTime repeats:NO];
+        
+        UNNotificationRequest* request = [UNNotificationRequest requestWithIdentifier:kNotificationIdentifier
+                                                                              content:content trigger:trigger];
+        
+        [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+            NSLog(@"completed!");
+        }];
+    }
+}
+
+#pragma mark - register APNs
+
+- (void) setupRemoteNotification:(UIApplication *)application {
+    [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
+    
+    // Push notification
+    if( SYSTEM_VERSION_LESS_THAN( @"10.0" ) )
+    {
+        if([application respondsToSelector:@selector(registerUserNotificationSettings:)])
+        {
+            // For ios 8 and later
+            UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound) categories:nil];
+            [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+            [[UIApplication sharedApplication] registerForRemoteNotifications];
+            
+        }
+        else
+        {
+            // For ios 7 and earlier
+            [[UIApplication sharedApplication] registerForRemoteNotificationTypes: (UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)];
+        }
+        
+        
+    }
+    else
+    {
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+        center.delegate = self;
+        [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error)
+         {
+             if( !error )
+             {
+                 [[UIApplication sharedApplication] registerForRemoteNotifications];
+                 NSLog( @"Push registration success - iOS 10" );
+             }
+             else
+             {
+                 NSLog( @"Push registration FAILED - iOS 10" );
+                 NSLog( @"ERROR: %@ - %@", error.localizedFailureReason, error.localizedDescription );
+                 NSLog( @"SUGGESTIONS: %@ - %@", error.localizedRecoveryOptions, error.localizedRecoverySuggestion );
+             }
+         }];
+    }
+}
 
 @end
